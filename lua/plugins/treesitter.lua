709543ff456426520c4b5c704d3c34810e47ed0f
Jake <jakescaife@pm.me>
2024-05-17 21:22:49 +0100

    Add treesitter for syntax highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
