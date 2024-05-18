ccf58691d087053a28ab955fcdd7ece0c8ca1568
Jake <jakescaife@pm.me>
2024-05-18 10:02:10 +0100

    Add telescope selection ui element

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,26 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local telescope = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", telescope.find_files)
-		vim.keymap.set("n", "<leader>fg", telescope.live_grep)
-	end,
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", telescope.find_files)
+			vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+			telescope.load_extension("ui-select")
+		end,
+	}
 }
