cdc923067a44e92fa640df62ff077f4de40f75a5
Jake <jakescaife@pm.me>
2024-05-18 10:33:59 +0100

    Add convenience keymaps

---
+++ lua/remaps.lua
@@ -0,0 +1,10 @@
+-- Move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Keep cursor in position
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
