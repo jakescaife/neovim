fde730459095f27f014aa7d48cf45f1e17fad436
Jake Scaife <jakescaife@pm.me>
2025-02-16 15:57:16 +0000

    Remove unnecessary plugins file

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,38 +1,21 @@
 vim.opt.guicursor = ""
 vim.g.mapleader = " "
-
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.inccommand = "nosplit"
-
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
-
 vim.opt.laststatus = 0
-
-vim.diagnostic.config({
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = true,
-})
