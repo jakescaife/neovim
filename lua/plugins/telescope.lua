889694a67f99ee8df03efcc5559ee9007ad04ff7
Jake Scaife <jakescaife@pm.me>
2025-02-22 18:53:20 +0000

    Add multigrep for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,33 +2,33 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+    },
     config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files)
-      vim.keymap.set("n", "<leader>fs", builtin.live_grep)
-      vim.keymap.set("n", "<leader>fb", builtin.buffers)
-      vim.keymap.set("n", "<leader>fg", builtin.git_files)
-
       local telescope = require("telescope")
       local actions = require("telescope.actions")
-      telescope.setup({
+      telescope.setup {
         defaults = {
-          layout_strategy = "vertical",
-          layout_config = {
-            vertical = {
-              preview_height = 0.5,
-              results_width = 0.8,
-            },
-          },
-          mappings = {
-            n = {
-              ["<leader>d"] = actions.delete_buffer,
-            }
-          },
-          prompt_position = "top",
+          mappings = { n = { ["<leader>d"] = actions.delete_buffer, } },
         },
-      })
+        pickers = { find_files = { theme = "ivy" }, },
+        extensions = {
+          fzf = {}
+        }
+      }
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fb", builtin.buffers)
+      vim.keymap.set("n", "<leader>fp", builtin.git_files)
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files { cwd = vim.fn.stdpath("config") }
+      end)
+
+      telescope.load_extension("fzf")
+      require "custom.telescope.multigrep".setup()
     end,
   },
   {
