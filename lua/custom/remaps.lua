b719ef04469590e60488dead3769ceea06774a1f
Jake Scaife <jakescaife@pm.me>
2025-02-22 17:45:08 +0000

    Add lspconfig with lua language server

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -2,3 +2,11 @@
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Lsp Configuration
+vim.keymap.set("n", "grn", vim.lsp.buf.rename)
+vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
+vim.keymap.set("n", "grr", vim.lsp.buf.references)
+vim.keymap.set("n", "gri", vim.lsp.buf.implementation)
+vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol)
+vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help)
