bb9132131a7a50d0687369116c7834aa9cc4095d
Jake Scaife <jakescaife@pm.me>
2025-03-08 20:56:31 +0000

    Reopen terminal buffer instead of creating new

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -8,9 +8,14 @@ vim.keymap.set("n", "grn", vim.lsp.buf.rename)
 vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
 vim.keymap.set("n", "grr", vim.lsp.buf.references)
 vim.keymap.set("n", "gri", vim.lsp.buf.implementation)
+vim.keymap.set("n", "grd", vim.lsp.buf.definition)
+vim.keymap.set("n", "grh", vim.lsp.buf.declaration)
 vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol)
 vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help)
 
 -- Quickfix list
 vim.keymap.set("n", "]q", "<cmd>cnext<CR>")
 vim.keymap.set("n", "[q", "<cmd>cprev<CR>")
+
+-- Terminal remaps
+vim.keymap.set("t", "<C-d>", "<C-\\><C-n>")
--- lua/custom/terminal.lua
+++ lua/custom/terminal.lua
@@ -6,9 +6,17 @@ vim.api.nvim_create_autocmd("TermOpen", {
   end,
 })
 
+local terminal_buffer = nil;
+
 vim.keymap.set("n", "<leader>st", function()
   vim.cmd.vnew()
-  vim.cmd.term()
+
+  if not terminal_buffer or not vim.api.nvim_buf_is_valid(terminal_buffer) then
+    vim.cmd.term();
+    terminal_buffer = vim.fn.bufnr("%")
+  end
+
+  vim.cmd("buffer" .. terminal_buffer)
   vim.cmd.wincmd("J")
   vim.api.nvim_win_set_height(0, 15)
 end)
